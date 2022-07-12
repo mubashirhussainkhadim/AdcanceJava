22dd27849a6520dd93c295bd8111f4acc517ec5f
Mubashir hussain <66620586+mubashirhussainkhadim@users.noreply.github.com>
2022-07-13 00:07:01 +0500

    Update customClass.java

--- customClass.java
+++ customClass.java
@@ -2,12 +2,14 @@
 class Employee {
     String NIC;
     int id;
-    String name;
+    String Proffesion;
+    int sallary;
 
     public void printdetails() {
         System.out.println("My Id is " + id);
-        System.out.println("My NAME IS " + name);
+        System.out.println("My Proffesion IS " + Proffesion);
         System.out.println("and MY NIC is " + NIC);
+        System.out.println("My Sallary is " + sallary);
     }
 }
 
@@ -15,11 +17,20 @@ public class customClass {
     public static void main(String[] args) {
         System.out.println("This is Our Custom Class");
         Employee mubashir = new Employee();
+        Employee Amina = new Employee();
         mubashir.NIC = "3520067786327";
-        mubashir.name = "Blockchain Scientist";
+        mubashir.Proffesion = "Blockchain Scientist";
         mubashir.id = 12;
+        mubashir.sallary = 120000000;
+        Amina.NIC = "35494985985";
+        Amina.Proffesion = "Graphic Designer";
+        Amina.id = 2;
+        Amina.sallary = 232200;
+
         System.out.println("Employer Data");
         mubashir.printdetails();
+        System.out.println("---------------------");
+        Amina.printdetails();
     }
 
 }
